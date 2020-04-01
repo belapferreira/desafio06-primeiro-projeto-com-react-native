321e40cfb8a2d132312e5e3c316cfc99ae3d10b5
Leonardo Vargas <leu1607@hotmail.com>
2020-04-01 20:07:34 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Main from './pages/Main';
@@ -8,22 +9,24 @@ const Stack = createStackNavigator();
 
 export default function Routes() {
   return (
-    <Stack.Navigator
-      headerBackTitleVisible={false}
-      headerLayoutPreset="center"
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: '#7159c1',
-        },
-        headerTintColor: '#FFF',
-      }}
-    >
-      <Stack.Screen name="Main" component={Main} options={{ title: 'Main' }} />
-      <Stack.Screen
-        name="User"
-        component={User}
-        options={({ route }) => ({ title: route.params.user.name })}
-      />
-    </Stack.Navigator>
+    <NavigationContainer>
+      <Stack.Navigator
+        headerBackTitleVisible={false}
+        headerLayoutPreset="center"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#7159c1',
+          },
+          headerTintColor: '#FFF',
+        }}
+        >
+        <Stack.Screen name="Main" component={Main} options={{ title: 'Main' }} />
+        <Stack.Screen
+          name="User"
+          component={User}
+          options={({ route }) => ({ title: route.params.user.name })}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
